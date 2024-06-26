40cb0d6b5649ab0dfa5640b77571c870055c3ff0
shafayet <shafayet.islam.61@gmail.com>
2024-06-27 00:11:16 +0930

    get and insert users

--- database/user.js
+++ database/user.js
@@ -1,5 +1,35 @@
 
 import { pool } from './connection.js';
 
-const result = await pool.query("SELECT * from Users");
-console.log(result);
+
+async function getUsers(){
+    const rows = await pool.query("SELECT * from Users");
+    return rows[0];
+}
+
+async function getUser(id){
+    const rows = await pool.query(`
+        SELECT * 
+        from Users
+        WHERE user_id = ? 
+        `, [id]);
+    return rows[0];
+}
+
+async function insertUser(email,password){
+    const result = await pool.query(`
+        INSERT INTO Users (email, password)
+        VALUES (?,?)  
+    `, [email, password]);
+
+    return result;
+}
+
+
+// const result = await insertUser("test02@gmail.com", "mypass02");
+// console.log(result[0]);
+
+// const rows = await getUsers();
+// console.log(rows);
+
+export { getUsers, getUser, insertUser }
